503def7dd380ca2cbef7252694576e54367eb4af
sigatapu <136946744+sigatapu@users.noreply.github.com>
2025-01-05 11:59:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Allow access from outside localhost
     port: 3000, // Or any other port of your choice
   },
-  base:'portfolio4',
+  base: '/vite-react-portfolio/', // Ensure this matches your repo name
 })
